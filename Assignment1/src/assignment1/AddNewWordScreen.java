3dbbf142edbd86cb31552f21ee2a8cdaae579b96
susantlalshrestha <susantlalshrestha@gmail.com>
2023-02-04 12:53:53 -0500

    added: todo in add new word screen

--- Assignment1/src/assignment1/AddNewWordScreen.java
+++ Assignment1/src/assignment1/AddNewWordScreen.java
@@ -16,5 +16,7 @@ public class AddNewWordScreen extends JPanel {
 
 	private void initUI() {
 		this.setBackground(Color.WHITE);
+		// TODO: Create GUI list all the words in JList and take input from user in JTextField and when pressed add button it should this function
+		// this.datasource.addNewWord("asdfhl");
 	}
 }
