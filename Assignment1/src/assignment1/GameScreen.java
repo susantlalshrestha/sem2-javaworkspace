dfce0f621da482d03fc9fa193e195e323d0c63dd
susantlalshrestha <susantlalshrestha@gmail.com>
2023-02-03 19:31:52 -0500

    changed: match letter logic

--- Assignment1/src/assignment1/GameScreen.java
+++ Assignment1/src/assignment1/GameScreen.java
@@ -31,17 +31,18 @@ public class GameScreen extends JPanel {
 			// TODO: show error message for already guessed letter
 			return;
 		}
-		if (randomWord.contains(String.valueOf(letter))) {
-			char[] guessedChars = guessedWord.toCharArray();
-			for (int i = 0; i < randomWord.length(); i++) {
-				if (randomWord.charAt(i) == letter) {
-					guessedChars[i] = letter;
-				}
-			}
-			guessedWord = new String(guessedChars);
-		} else {
+		if (!randomWord.contains(String.valueOf(letter))) {
 			this.livesLeft -= 1;
 			// TODO: Refresh the lives left in the ui
+			return;
 		}
+		char[] guessedChars = guessedWord.toCharArray();
+		for (int i = 0; i < randomWord.length(); i++) {
+			if (randomWord.charAt(i) == letter) {
+				guessedChars[i] = letter;
+			}
+		}
+		guessedWord = new String(guessedChars);
+
 	}
 }
