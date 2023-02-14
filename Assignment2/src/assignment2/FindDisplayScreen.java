08e3a66a2107601b079d9e65b17f58a983f2a4f6
susantlalshrestha <susantlalshrestha@gmail.com>
2023-02-14 13:15:47 -0500

    changed: layout of hte find and display screen

--- Assignment2/src/assignment2/FindDisplayScreen.java
+++ Assignment2/src/assignment2/FindDisplayScreen.java
@@ -1,6 +1,5 @@
 package assignment2;
 
-import javax.swing.JFrame;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.*;
@@ -29,38 +28,25 @@ public class FindDisplayScreen extends JFrame {
 		this.setTitle("Find/Display Products");
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setSize(AppConstants.APP_FRAME_WIDTH, AppConstants.APP_FRAME_HEIGHT);
-		this.setSize(800, 500);
+//		this.setSize(800, 500);
 		this.setResizable(false);
 		this.setLocation(200, 200);
-
-		JPanel radioButtonPanel = new JPanel();
-		radioButtonPanel.setLayout(new BoxLayout(radioButtonPanel, BoxLayout.Y_AXIS));
 
 		allRadioButton = new JRadioButton("All");
 		keywordRadioButton = new JRadioButton("Keyword");
 		priceRangeRadioButton = new JRadioButton("Price Range");
 
 		ButtonGroup radioButtonGroup = new ButtonGroup();
-		radioButtonGroup.add(allRadioButton);
-		radioButtonGroup.add(keywordRadioButton);
 		radioButtonGroup.add(priceRangeRadioButton);
+		radioButtonGroup.add(keywordRadioButton);
+		radioButtonGroup.add(allRadioButton);
 
-		radioButtonPanel.add(allRadioButton);
-		radioButtonPanel.add(keywordRadioButton);
-		radioButtonPanel.add(priceRangeRadioButton);
-
-		JPanel searchPanel = new JPanel();
-		searchPanel.setLayout(new GridLayout(1, 4, 10, 0));
-
-		toPriceTextField = new JTextField(3);
+		toPriceTextField = new JTextField(8);
 		toPriceTextField.setText("to");
-		toPriceTextField.setFont(toPriceTextField.getFont().deriveFont(10f));
-		fromPriceTextField = new JTextField(3);
+		fromPriceTextField = new JTextField(8);
 		fromPriceTextField.setText("from");
-		fromPriceTextField.setFont(fromPriceTextField.getFont().deriveFont(10f));
-		keywordTextField = new JTextField(3);
+		keywordTextField = new JTextField(8);
 		keywordTextField.setText("keyword");
-		keywordTextField.setFont(keywordTextField.getFont().deriveFont(10f));
 		searchButton = new JButton("Find/Display");
 
 		searchButton.addActionListener(new ActionListener() {
@@ -98,18 +84,32 @@ public class FindDisplayScreen extends JFrame {
 				}
 			}
 		});
-		searchPanel.add(fromPriceTextField);
-		searchPanel.add(toPriceTextField);
-		searchPanel.add(keywordTextField);
-		searchPanel.add(searchButton);
 
 		productTable = new JTable();
+		productTable.setEnabled(false);
 		JScrollPane scrollPane = new JScrollPane(productTable);
 
-		this.getContentPane().setLayout(new BorderLayout());
-		this.getContentPane().add(radioButtonPanel, BorderLayout.WEST);
-		this.getContentPane().add(searchPanel, BorderLayout.NORTH);
-		this.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new FlowLayout(FlowLayout.LEFT));
+		panel1.add(priceRangeRadioButton);
+		panel1.add(fromPriceTextField);
+		panel1.add(toPriceTextField);
+		panel1.add(searchButton);
+
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(new FlowLayout(FlowLayout.LEFT));
+		panel2.add(keywordRadioButton);
+		panel2.add(keywordTextField);
+
+		JPanel panel3 = new JPanel();
+		panel3.setLayout(new FlowLayout(FlowLayout.LEFT));
+		panel3.add(allRadioButton);
+
+		this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+		this.getContentPane().add(panel1);
+		this.getContentPane().add(panel2);
+		this.getContentPane().add(panel3);
+		this.getContentPane().add(scrollPane);
 		this.setVisible(true);
 	}
 }
